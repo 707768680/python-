5679acf53689daf805009488cbcbf846b38c05b2
707768680 <707768680@qq.com>
2018-09-20 20:21:53 +0800

    1

--- 多任务/生成器.py
+++ 多任务/生成器.py
@@ -2,16 +2,17 @@ def create_num(all_num):
     current_num = 0
     a, b = 0, 1
     while current_num < all_num:
-        yield a
+        ret = yield a
+        print("====ret====>", ret)
         a, b = b, a+b
         current_num += 1
 
 obj = create_num(10)
 
-print(next(obj))
-print(next(obj))
-print(next(obj))
-print(next(obj))
-print(next(obj))
-print(next(obj))
-print(next(obj))
+ret = next(obj)
+print(ret)
+
+# 用生成器对象.send方法可以传参数,用 yeild 返回值来接收,
+# 一般不用于第一次执行,如果用于第一次则传 None 参数否则报错 
+ret = obj.send("hahaha")
+print(ret)
